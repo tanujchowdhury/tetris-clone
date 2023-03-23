ecb564573f2086234c4c081cd94ab592a7342751
Bryan McKernon <bryanmckernon@d.local>
2023-03-22 17:10:29 -0700

    Before data model change

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -4,7 +4,12 @@ import Cell from "../Cell/Cell";
 
 const Board = () => {
   const [BoardCellState, setBoardCellState] = useState({});
-  const [Shape, setShape] = useState({ 4: "green", 5: "green", 14: "green", 15: "green" });
+  const [Shape, setShape] = useState({
+    4: "green",
+    5: "green",
+    14: "green",
+    15: "green",
+  });
 
   const updateBoardState = (newState) => {
     setBoardCellState({ ...BoardCellState, ...newState });
@@ -16,50 +21,71 @@ const Board = () => {
     while (i < numberOfCells) {
       cells[i] = cellColor;
       i++;
-    } 
+    }
     return updateBoardState(cells);
-  }
+  };
 
   const renderShape = (newShape) => {
     setShape(newShape);
-  }
+  };
 
   const dropShape = () => {
     const updatedShape = {};
     for (let i in Object.keys(Shape)) {
-      updatedShape[(Number(Object.keys(Shape)[i]) + 10)] = "green";
+      updatedShape[Number(Object.keys(Shape)[i]) + 10] = "green";
     }
-    
+
     //turn the old shape blue
     const oldShape = {};
     for (let i in Object.keys(Shape)) {
-      oldShape[(Number(Object.keys(Shape)[i]))] = "blue";
+      oldShape[Number(Object.keys(Shape)[i])] = "blue";
     }
 
+    isCollidedShape();
+
     setShape(updatedShape);
-    updateBoardState({...oldShape, ...updatedShape});
-  }
+    updateBoardState({ ...oldShape, ...updatedShape });
+  };
+
+  console.log(BoardCellState);
+
+  const isCollidedShape = () => {
+    //collisions occur if 1)shape reaches end of board 2) shape reaches merged shape
+    const shapeIndices = Object.keys(Shape).map((shapeIndex) => {
+      return Number(shapeIndex);
+    });
+
+    // const shapeLowestPoint = Math.max(...shapeIndices);
+  };
+
+  const mergeShape = () => {};
 
   useEffect(() => {
     createBoard(200, "blue");
   }, []);
-  
+
   useEffect(() => {
     renderShape({ 4: "green", 5: "green", 14: "green", 15: "green" });
-  }, [])
+  }, []);
 
   useEffect(() => {
     const myInterval = setInterval(dropShape, 1500);
     return () => {
       clearInterval(myInterval);
-    }
+    };
   });
 
   return (
     <div>
       <button
         onClick={() => {
-          updateBoardState({ ...BoardCellState, 0: "green", 1: "green", 10: "green", 11: "green" });
+          updateBoardState({
+            ...BoardCellState,
+            0: "green",
+            1: "green",
+            10: "green",
+            11: "green",
+          });
         }}
       >
         Update the state
